13abe80680fab234498f7d79b8ef0ebc03e66c36
Flinn <fmueller@flinns-macbook-pro.local>
2009-02-20 23:34:17 -0500

    additional specs

--- spec/integration/tyrant_adapter_spec.rb
+++ spec/integration/tyrant_adapter_spec.rb
@@ -13,6 +13,12 @@ class ::Heffalump
   property :description, String
 end
 
+def create_heffalumps
+  [::Heffalump.create(:id => '1', :title => "Purple", :description => "this is one"),
+   ::Heffalump.create(:id => '2', :title => "Pink",   :description => "this is two"),
+   ::Heffalump.create(:id => '3', :title => "Blue",   :description => "this is three")]
+end
+
 describe DataMapper::Adapters::TyrantAdapter do
   before do
     DataMapper.setup(:tyrant, :adapter => "tyrant")
@@ -38,15 +44,34 @@ describe DataMapper::Adapters::TyrantAdapter do
     ::Heffalump.get(@heffalump.id).should == @heffalump
   end
 
-  it "should successfully find all objects" do
-    heffalumps = [::Heffalump.create(:id => '1', :title => "Purple", :description => "this is one"),
-                  ::Heffalump.create(:id => '2', :title => "Pink",   :description => "this is two"),
-                  ::Heffalump.create(:id => '3', :title => "Blue",   :description => "this is three")]
+  it "should successfuly delete an object" do
+    @heffalump.destroy.should == true
+  end
 
+  it "should successfully find all objects" do
+    heffalumps = create_heffalumps
     ::Heffalump.all.should == heffalumps
   end
 
-  it "should successfuly delete an object" do
-    @heffalump.destroy.should == true
+  it "should successfully find all objects by a property" do
+    heffalumps = create_heffalumps
+    ::Heffalump.all(:title => "Purple").should == heffalumps[1]
+  end
+
+  # OPTIMIZE: These are probably really poorly written specs
+  it "should order by a property when finding objects" do
+    heffalumps = create_heffalumps
+
+    ::Heffalump.all(:order => [:id.asc]).should == heffalumps
+    ::Heffalump.all(:order => [:id.desc]).should == heffalumps.reverse
+
+    heffalumps_by_title = heffalumps.sort{ |x,y| x.title <=> y.title }
+    ::Heffalump.all(:order => [:title.asc]).should == heffalumps_by_title
+    ::Heffalump.all(:order => [:title.desc]).should == heffalumps_by_title.reverse
+  end
+
+  it "should limit found objects" do
+    create_heffalumps
+    ::Heffalump.all(:limit => 2).size.should == 2
   end
 end
