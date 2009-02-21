1a03e08d11ae05e76c77c29448a44fe4396ea652
Flinn <fmueller@flinns-macbook-pro.local>
2009-02-20 22:56:07 -0500

    more specs

--- spec/integration/tyrant_adapter_spec.rb
+++ spec/integration/tyrant_adapter_spec.rb
@@ -1,29 +1,52 @@
 require 'pathname'
 require Pathname(__FILE__).dirname.parent.join('spec.rb')
 
-describe DataMapper::Adapters::TyrantAdapter do
+class ::Heffalump
+  include DataMapper::Resource
 
+  def self.default_repository_name
+    :tyrant
+  end
+
+  property :id,          String, :key => true
+  property :title,       String
+  property :description, String
+end
+
+describe DataMapper::Adapters::TyrantAdapter do
   before do
     DataMapper.setup(:tyrant, :adapter => "tyrant")
-
-    class ::Heffalump
-      include DataMapper::Resource
-
-      def self.default_repository_name
-        :tyrant
-      end
-
-      property :title, String, :key => true
-      property :description, String
-    end
-
-    @heff1 = Heffalump.create(:title => "Purple", :description => "this is one")
-    @heff2 = Heffalump.create(:title => "Blue",   :description => "this is two")
-    @heff2 = Heffalump.create(:title => "Pink",   :description => "this is three")
+    Heffalump.all.destroy!
+    @heffalump = ::Heffalump.create(:id => '1', :title => "Purple", :description => "this is one")
   end
 
   it "should successfully save an object" do
-    @heff1.new_record?.should == false
+    @heffalump.new_record?.should == false
   end
-  
+
+  it "should successfully update an object" do
+    @heffalump.title = 'Green'
+    @heffalump.save.should == true
+  end
+
+  it "should successfully update an object's attributes" do
+    @heffalump.update_attributes(:title => 'Green').should == true
+    Heffalump.get(@heffalump.id).title.should == 'Green'
+  end
+
+  it "should successfully find a single object" do
+    ::Heffalump.get(@heffalump.id).should == @heffalump
+  end
+
+  it "should successfully find all objects" do
+    heffalumps = [::Heffalump.create(:id => '1', :title => "Purple", :description => "this is one"),
+                  ::Heffalump.create(:id => '2', :title => "Pink",   :description => "this is two"),
+                  ::Heffalump.create(:id => '3', :title => "Blue",   :description => "this is three")]
+
+    ::Heffalump.all.should == heffalumps
+  end
+
+  it "should successfuly delete an object" do
+    @heffalump.destroy.should == true
+  end
 end
